d2a74dba7fa2edd24d52e9149c2c8dab60dd6611
emsi06001 <emsi06001@E-PW0234N3>
2023-10-06 10:29:44 +0200

    Add project files.

---
+++ inluppg3.6/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace inluppg3_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv ditt namn");
+
+            string input = Console.ReadLine();
+            int spaceIndex = input.IndexOf(' ');
+            string fornamn = input[..spaceIndex];
+            string efternamn = input[(spaceIndex+1)..];
+
+            if (fornamn.CompareTo(efternamn) == -1)
+            {
+                Console.WriteLine("Förnamnet kommer före i alfabetisk ordning");
+            }
+            else if (fornamn.CompareTo(efternamn) == 1)
+            {
+                Console.WriteLine("Förnamnet kommer efter i alfabetisk ordning");
+            }
+
+
+
+
+        }
+    }
+}
